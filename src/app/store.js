0c1779976c2f608db2607e2565acba82f3605ea1
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-06-27 11:16:22 +0000

    created topicsSlice and updated components

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import topicReducer from "../features/topics/topicsSlice"
+
+export default configureStore({
+  reducer: {
+    topics: topicReducer,
+  },
+});
